e7e60fccc0e23811214af5b2c41c8e3fc3683508
pranjalrawat <rawatpranjal111@gmail.com>
2020-11-29 14:10:13 +0530

    update resume

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -124,4 +124,4 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
