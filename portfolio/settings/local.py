db47feace82c6591975f3d4f2def3a53aaa3540e
Linux <rawatpranjal111@gmail.com>
2021-06-20 13:15:28 +0530

    static

--- portfolio/settings/local.py
+++ portfolio/settings/local.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['pranjalrawat.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
