24706f84911297d4a53ae61dcd863e1184b40107
Linux <rawatpranjal111@gmail.com>
2021-06-20 13:48:56 +0530

    requirement

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = environ('SECRET_KEY')
+SECRET_KEY = 'django-insecure-xz@ibfe)hm_h*vmq^z2-sj-$o&(+1*ry^womszv3#!&0eg&08^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
