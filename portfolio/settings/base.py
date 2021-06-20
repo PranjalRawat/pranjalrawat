12a92fe8bbe94d25176f957d3364d4200c198717
Linux <rawatpranjal111@gmail.com>
2021-06-20 16:25:58 +0530

    setting

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
