b2b3e2961640deec54fb1f1ee70823e4997f6144
Linux <rawatpranjal111@gmail.com>
2021-06-20 12:49:28 +0530

    runtime

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -128,7 +128,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
