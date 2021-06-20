d8e319fe6e8ccb7ffd6d1cc4edd8943e6fa97903
Linux <rawatpranjal111@gmail.com>
2021-06-20 12:52:10 +0530

    whitenoise

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
