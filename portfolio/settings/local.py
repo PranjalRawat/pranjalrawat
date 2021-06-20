ffa7d0aa4e7f9e44c665d18737e8d9f75c6c9cae
Pranjal_rawat <34087626+PranjalRawat@users.noreply.github.com>
2021-06-20 16:33:55 +0530

    Update local.py

--- portfolio/settings/local.py
+++ portfolio/settings/local.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['pranjalrawat.herokuapp.com', '127.0.0.1']
 
