db31eb8df23c43dd8b91fa3155d1c53468e7d5ce
Linux <rawatpranjal111@gmail.com>
2021-07-28 13:46:44 +0530

    changes

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'modelapp',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -3,12 +3,6 @@ from portfolio.settings.base import INSTALLED_APPS, MIDDLEWARE
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-INSTALLED_APPS+= [
-    'whitenoise.runserver_nostatic',
-]
-MIDDLEWARE+=[
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
 
 DEBUG = False
 ALLOWED_HOSTS = ['pranjalrawat.herokuapp.com']
